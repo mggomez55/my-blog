8459d0614be87ced1e7bf0617265e6329e4ded76
mggomez55 <mggomez55@gmail.com>
2022-08-10 13:28:59 +0100

    my first commit

---
+++ src/build/precache-manifest.a53fd67cf5ebc38d2bc3b523ec543755.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdadbede8e9b6407c890",
+    "url": "/static/css/main.ce6ebf0f.chunk.css"
+  },
+  {
+    "revision": "cdadbede8e9b6407c890",
+    "url": "/static/js/main.ca55cb9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7125d3c337e9d104bda",
+    "url": "/static/js/2.f8b8cd58.chunk.js"
+  },
+  {
+    "revision": "3f98eaa7832991bc455d39300cf2cbd2",
+    "url": "/index.html"
+  }
+];
